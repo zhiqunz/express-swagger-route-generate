a161d6c3bce7dea87f76c1ca0b2fc36fcaa52910
zhuzhiqun <bgt55tgb>
2019-03-15 01:04:46 +0800

    add eslint and log4js

---
+++ main.js
@@ -0,0 +1,10 @@
+const log = require('./log');
+
+log.info('*** start now ***');
+
+log.info('*** get config file ***');
+
+log.info('**** create the route.js ***');
+log.info('****  ***');
+
+log.info('*** start end ***');
